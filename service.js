8b19655bd65ad70e3821a1b29cb64e229750d254
sunilchaudhary3112 <sunilchaudhary3112@gmail.com>
2019-01-28 01:36:27 +0530

    added amzn id

--- service.js
+++ service.js
@@ -20,9 +20,9 @@ module.exports = new function() {
   var metadata = {
     allowConfigUpdate: true, //set to false to turn off REST endpoint of allowing update of metadata
     waitForMoreResponsesMs: 200,  //milliseconds to wait for additional webhook responses
-    amzn_appId: "amzn1.ask.skill.6761dc5c-1df8-4bd7-b9f1-62bad5a7b582",
-    channelSecretKey: 'FmfrPvUvGNf0H3ZYvhXdkjtOtmI2Zqn6',
-    channelUrl: 'https://botfrk1I0024H8BCC65bots-mpaasocimt.botmxp.ocp.oraclecloud.com:443/connectors/v1/tenants/idcs-6d466372210e4300bb31f4db15e8e96c/listeners/webhook/channels/4A982611-EC67-489C-BD6D-7A630C04CF42'
+    amzn_appId: "amzn1.ask.skill.03e89841-78b3-4882-bb21-14b0fa95f920",
+    channelSecretKey: 'PmAPpVXfqKmDKst2Xwp6uk6tGSkYj36r',
+    channelUrl: 'https://b2b34218.ngrok.io/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/3743FDF7-5381-4C25-90F9-77DD57ED17FB'
   };
 
   this.randomIntInc = function (low, high) {
